9933da9b70218e9b3d5b6ecb60877b7bf0d91c6b
Maaz <maazejaz032@gmail.com>
2022-05-13 04:46:56 +0500

    Cards made responsive

--- src/views/UserHome.js
+++ src/views/UserHome.js
@@ -7,6 +7,7 @@ import MenuItem from '@mui/material/MenuItem';
 
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { Box } from '@mui/system';
 
 
 function UserHome() {
@@ -56,58 +57,72 @@ function UserHome() {
 
             <CustomerNavbar />
 
-            <Grid maxWidth={"95vw"} className='home-title-container'>
+            <Grid className='home-title-container' sx={{ marginBottom: { xs: "0" }, flexDirection: { xs: "column", sm: "row", md: "row", lg: "row" } }}>
 
-                <span className='title'>Order food</span>
+                <Box className='title' sx={{ textAlign: { xs: "center", sm: "left" }, m: 0 }}>Order food</Box>
 
-                <span>
+                <Box sx={{ width: { xs: "100%", sm: "fit-content" }, display: { xs: "flex" }, justifyContent: { xs: "flex-end" } }}>
+                    <Box >
+                        <FormControl variant="standard" sx={{ m: 1, minWidth: { xs: 70, sm: 100 }, }} size={"small"} className="filter-select">
+                            <InputLabel
+                                id="demo-simple-select-helper-label"
+                                sx={{ fontSize: { xs: 10, sm: 14 } }}
+                            >Category</InputLabel>
+                            <Select
+                                autoWidth={true}
+                                labelId="demo-simple-select-helper-label"
+                                id="demo-simple-select-helper"
+                                value={category}
+                                label="category"
+                                onChange={handleCategory}
+                                sx={{ borderColor: "rgb(230, 160, 70)", fontSize: { xs: 10, sm: 14 } }}
 
-                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }} size={"small"} className="filter-select">
-                        <InputLabel id="demo-simple-select-helper-label">Category</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-helper-label"
-                            id="demo-simple-select-helper"
-                            value={category}
-                            label="category"
-                            onChange={handleCategory}
+                            >
+                                <MenuItem value={"all"}>All</MenuItem>
+                                <MenuItem value={"desi"}>Desi</MenuItem>
+                                <MenuItem value={"bbq"}>BBQ</MenuItem>
+                                <MenuItem value={"chinese"}>Chinese</MenuItem>
+                                <MenuItem value={"fast food"}>Fast Food</MenuItem>
+                            </Select>
+                        </FormControl>
 
-                        >
-                            <MenuItem value={"all"}>All</MenuItem>
-                            <MenuItem value={"desi"}>Desi</MenuItem>
-                            <MenuItem value={"bbq"}>BBQ</MenuItem>
-                            <MenuItem value={"chinese"}>Chinese</MenuItem>
-                            <MenuItem value={"fast food"}>Fast Food</MenuItem>
-                        </Select>
-                    </FormControl>
+                        <FormControl sx={{ m: 1, minWidth: { xs: 70, sm: 100 }, }} variant="standard" size={"small"} className="filter-select">
+                            <InputLabel
+                                id="demo-simple-select-helper-label"
+                                sx={{ borderColor: "rgb(230, 160, 70)", fontSize: { xs: 10, sm: 14 } }}
+                            >Price</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-helper-label"
+                                id="demo-simple-select-helper"
+                                value={price}
+                                label="Price"
+                                onChange={handlePrice}
+                                sx={{ borderColor: "rgb(230, 160, 70)", fontSize: { xs: 10, sm: 14 } }}
 
-                    <FormControl sx={{ m: 1, minWidth: 120 }} variant="standard" size={"small"} className="filter-select">
-                        <InputLabel id="demo-simple-select-helper-label">Price</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-helper-label"
-                            id="demo-simple-select-helper"
-                            value={price}
-                            label="Price"
-                            onChange={handlePrice}
+                            >
+                                <MenuItem value={"low"}>Low to High</MenuItem>
+                                <MenuItem value={"high"}>High to Low</MenuItem>
 
-                        >
-                            <MenuItem value={"low"}>Low to High</MenuItem>
-                            <MenuItem value={"high"}>High to Low</MenuItem>
-
-                        </Select>
-                    </FormControl>
-
-                </span>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Box>
 
             </Grid>
 
+            <Box sx={{ p: { xs: "0", sm: "0px 8px", md: "0 8px", lg: "0 16px" , xl:"0 32px" } }}>
+                <Grid container className='cards-holder' columns={12} columnSpacing={{ xs: 0, sm: 4, md: 4, lg: 4 }} rowSpacing={4} sx={{ width: "90vw", mt:0 }}>
 
-            <Grid container className='cards-holder' maxWidth={"95vw"}>
-                {
-                    props.map((data) => (
-                        <MediaCard key={data.shop} {...data} />
-                    ))
-                }
-            </Grid>
+                    {
+                        props.map((data) => (
+                            <Grid item xs={10} sm={6} md={4} lg={3} key={data.shop}>
+                                <MediaCard  {...data} />
+                            </Grid>
+                        ))
+                    }
+
+                </Grid>
+            </Box>
         </Grid>
     )
 }
